3300950d0765e3eb187fa71dbfe42a8cf056a4cb
Samarth desai <Samarth3214@gmail.com>
2020-02-06 16:20:32 +0530

    Updates

---
+++ precache-manifest.6f35e9b4ccabd2206af320d55da5bda8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625d8ac82bddd5f96cc0434afe98f576",
+    "url": "/Artsell/index.html"
+  },
+  {
+    "revision": "2b2439bfb21987ebde98",
+    "url": "/Artsell/static/css/2.9720967d.chunk.css"
+  },
+  {
+    "revision": "cc703e1fbc48f9007ea8",
+    "url": "/Artsell/static/css/main.14ec70ea.chunk.css"
+  },
+  {
+    "revision": "2b2439bfb21987ebde98",
+    "url": "/Artsell/static/js/2.7c240a1d.chunk.js"
+  },
+  {
+    "revision": "05de66bb1438b720f47a2ef1a1b8ae11",
+    "url": "/Artsell/static/js/2.7c240a1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc703e1fbc48f9007ea8",
+    "url": "/Artsell/static/js/main.2c70c98f.chunk.js"
+  },
+  {
+    "revision": "8fe2957844d0647b92c8",
+    "url": "/Artsell/static/js/runtime-main.1b90f7d6.js"
+  }
+]);
